a0c8748dcc163ed007a041c02e166c1e7683a9c6
mauman <manmau@yahoo.com>
2020-10-21 17:29:28 +0100

    I added a small comment at the beginning of the file

--- StableFit.cpp
+++ StableFit.cpp
@@ -18,6 +18,8 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 
+/* this modification was done by Maurizio today*/
+
 #include "stdafx.h"
 #include "StableFit.h"
 #include <algorithm>
